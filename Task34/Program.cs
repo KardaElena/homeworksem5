f04072aaedf6850fae8dc65c7c51ffbabd125728
KardaElena <karda.elena@gmail.com>
2022-11-10 19:43:00 +0300

    A task about even numbers in array was added

---
+++ Task34/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задайте массив, заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] GetArray(int size, int a, int b)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(a, b);
+        Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+int EvenNumbers(int [] array)
+{
+    int [] arr = array;
+    int count = 0;
+    foreach (int el in arr)
+    {
+        if (el%2 == 0)
+        {
+        count++;
+        }
+    }
+    return count;
+}
+
+Console.Clear();
+int[] three = GetArray(10, 100, 999);
+Console.WriteLine();
+Console.WriteLine($"Количество чётных числе в массиве: {EvenNumbers(three)}");
